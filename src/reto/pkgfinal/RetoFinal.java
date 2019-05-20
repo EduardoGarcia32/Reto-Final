b1e4abc665f6e6de6f19de961183638a73fad631
EduardoGarcia32 <slender560@gmail.com>
2019-05-20 00:25:21 -0500

    Creacion de objetos

---
+++ src/reto/pkgfinal/RetoFinal.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package reto.pkgfinal;
+
+import java.util.Random;
+import java.util.Scanner;
+
+
+/**
+ *
+ * @author Eduardo Garcia
+ */
+public class RetoFinal {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        Scanner s = new Scanner(System.in);
+        int PoderHeroe = 2;
+        int PoderMago = 2;
+        
+        int HadaZ = 0;
+        int HadaJu = 0;
+        int Madera = 0;
+        int a,b,c = 0 ;
+        
+        String Nombre;
+        String Ok;
+        String Repito = "salir";
+       
+          
+        }
+    }
+    
+
